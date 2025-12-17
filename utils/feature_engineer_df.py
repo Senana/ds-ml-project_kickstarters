849d26128fd6512ecfe971c492d48c3480befc5e
Ievgen Perederieiev <peredereev.ievgen@gmail.com>
2025-12-17 10:57:40 +0100

    fix for pd.cut parameters for duration bins

--- utils/feature_engineer_df.py
+++ utils/feature_engineer_df.py
@@ -110,8 +110,10 @@ def build_features(
     # --- duration ---
     df["duration_bins"] = pd.cut(
         df["duration_days"],
-        bins=[15, 29, 45, 60, 75],
-        labels=["2 weeks", "4 weeks", "6 weeks", "8 weeks"]
+        bins=[0, 15, 29, 45, 60, 75, float("inf")],
+        labels=["<2 weeks", "2 weeks", "4 weeks", "6 weeks", "8+ weeks"],
+        right=True,
+        include_lowest=True
     )
 
     # --- backers ---
